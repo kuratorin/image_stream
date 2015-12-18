ab35665e2b2bc88247b60d6a6256641dfc019ff6
MartinOrtel <s49837@beuth-hochschule.de>
2015-12-18 17:54:31 +0100

    more  naise

--- image_stream.py
+++ image_stream.py
@@ -1,4 +1,5 @@
 import logging
+import time
 import os
 import modules.fourchan as fourchan
 import modules.fourchan.SiteComponents as SiteComponents
@@ -6,7 +7,7 @@ import tkinter
 from PIL import ImageTk, Image
 
 # create logger with 'spam_application'
-logger = logging.getLogger('debug')
+logger = logging.getLogger('info')
 logger.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
 # fh = logging.FileHandler('spam.log')
@@ -27,6 +28,7 @@ download_dir = "download"
 class App():
     def __init__(self):
         self.root = tkinter.Tk()
+        self.root.configure(background='black')
         image1=Image.open('test.jpg')
         self.root.geometry("{0}x{1}+0+0".format(self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
         self.tkpi=ImageTk.PhotoImage(image1)
@@ -35,6 +37,7 @@ class App():
 
         self.displayed_images = []
         self.update_image()
+        self.hide_image()
         self.root.mainloop()
 
 
@@ -42,12 +45,27 @@ class App():
         current_image=Image.open(self.get_image_path())
         self.tkpi=ImageTk.PhotoImage(current_image)
         label_image=tkinter.Label(self.root, image=self.tkpi)
+        label_image.configure(background='black')
         label_image.place(relx=0.5,rely=0.5,anchor='center')
 
         print("slide")
-        self.root.after(500, self.update_image)
+        self.root.after(2000, self.update_image)
+
+    def hide_image(self):
+        current_image=Image.open("black.jpg")
+        self.tkpi=ImageTk.PhotoImage(current_image)
+        label_image=tkinter.Label(self.root, image=self.tkpi)
+        label_image.configure(background='black')
+        label_image.place(relx=0.5,rely=0.5,anchor='center')
+
+        print("hidden")
+        self.root.after(2000, self.hide_image)
 
     def get_image_path(self):
+        if len(self.displayed_images) > 100:
+            logger.info("len(self.displayed_images) = {}".format(len(self.displayed_images)))
+            self.displayed_images = self.displayed_images[9:-1]
+            logger.info("len(self.displayed_images) = {}".format(len(self.displayed_images)))
         for file_path in os.listdir("./"):
             if file_path.endswith(".jpg"):
                 if file_path not in self.displayed_images:
@@ -90,8 +108,9 @@ def main():
                 while jpg:
                     logger.info("popping jpg {}".format(jpg))
                     jpg = thread.pop_jpg()
+                    time.sleep(0.1)
                     # gui.display(jpg)
 
-app = App()
+#app = App()
 setup()
 main()
